77c380b6f001deb6e0de3dbf0248a8e5b2286aa5
Layxe <layxe.alex@gmail.com>
2020-10-25 12:16:55 +0100

    initial commit

---
+++ src/de/alexander/skilocationtracker/Main.java
@@ -0,0 +1,17 @@
+package de.alexander.skilocationtracker;
+
+/**
+ * Main class for the application
+ *
+ * @author Alexander Niedermayer
+ * @version 1.0
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        // TODO Everything
+
+    }
+
+}
